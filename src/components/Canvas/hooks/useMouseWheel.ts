291784a4da655fa29f19719157dec3ee9082030b
sid.shi <18262272638@163.com>
2021-09-02 18:00:56 +0800

    add mouse wheel

--- src/components/Canvas/hooks/useMouseWheel.ts
+++ src/components/Canvas/hooks/useMouseWheel.ts
@@ -1,48 +1,17 @@
-export default function useMouseWheel() {
-  return null;
-}
+const handleForMouseWheel = (canvas: any) => {
+  console.log(">>>> canvas", canvas);
+  const onMousewheel = (event: any) => {
+    var delta = event.e.deltaY;
+    var zoom = canvas.getZoom();
+    zoom *= 0.999 ** delta;
+    if (zoom > 20) zoom = 20;
+    if (zoom < 0.01) zoom = 0.01;
+    canvas.setZoom(zoom);
+    event.e.preventDefault();
+    event.e.stopPropagation();
+  };
 
-// import { setCanvasAttrs } from "@store/actions";
-// import { debounce } from "lodash-es";
-// import { useContext, useEffect, useRef } from "react";
+  canvas && canvas.on("mouse:wheel", onMousewheel);
+};
 
-// export const useMouseWheel = (canvas, loading: boolean) => {
-//   useEffect(() => {
-//     const onMousewheel = (event: any) => {
-//       event.stopPropagation();
-
-//       if (loading) return;
-//       const { wheelDelta } = event;
-//       const instance = (context as any).instance();
-//       let newCanvasScale = rounding(instance.canvasScaling);
-//       if (!instance || !newCanvasScale) return;
-
-//       const step = 0.015;
-//       if (wheelDelta > 0) {
-//         newCanvasScale = newCanvasScale + step;
-//       } else {
-//         newCanvasScale = newCanvasScale - step;
-//       }
-//       if (rounding(instance.canvasScaling) === newCanvasScale) {
-//         return;
-//       }
-//       const fixScale = newCanvasScale < 0.05 ? 0.05 : newCanvasScale;
-
-//       instance.canvasScaling = fixScale;
-//       console.log(">>> instance", instance);
-//       instance.setZoom(fixScale);
-
-//     };
-//     (canvasRef.current as any)?.addEventListener(
-//       "mousewheel",
-//       debounce(onMousewheel, 200)
-//     );
-
-//     const canvasContainer = canvasRef?.current;
-//     return () => {
-//       (canvasContainer as any)?.removeEventListener("mousewheel", onMousewheel);
-//     };
-//   }, [loading]);
-
-//   return { canvasRef };
-// };
+export default handleForMouseWheel;
--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { fabric } from "fabric";
 import useCustomizationHandler from "./hooks/useCustomizationHandler";
+import handleForMouseWheel from "./hooks/useMouseWheel";
 
 export default function Canvas() {
+  const [myCanvas, setMyCanvas] = useState(null);
+
   useCustomizationHandler();
 
   useEffect(() => {
     const canvas = new fabric.Canvas("my-fabric-canvas");
+    setMyCanvas(myCanvas);
     const textbox = new fabric.Textbox("中，文。会自动换行吗,加了配置就会了", {
-    // const textbox = new fabric.Textbox("年(", {
+      // const textbox = new fabric.Textbox("年(", {
       fontSize: 20,
       top: 300,
       left: 800,
@@ -24,6 +28,8 @@ export default function Canvas() {
     // canvas.add(text);
     canvas.add(textbox);
     canvas.renderAll();
+
+    handleForMouseWheel(canvas);
 
     // UseEffect's cleanup function
     return () => {
